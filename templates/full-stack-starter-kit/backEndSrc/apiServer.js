7ac396a349babe808b5ad16b15ab3c57c8ebd46f
Arthur <arthur.bernier@blavity.com>
2019-12-18 17:58:27 -0500

    Add full stack stuff

---
+++ templates/full-stack-starter-kit/backEndSrc/apiServer.js
@@ -0,0 +1,7 @@
+// this is the file you use to start your api building
+
+// This replaces your server.js but you still do everything exactly the same
+
+// in this folder you should have a models folder, controllers and any db files you need.
+
+// you will still tell this file to listen for server requests.
