6aee29dc120cf266d3b4933a9eab008f201d58f6
Arthur <arthur.bernier@blavity.com>
2019-12-20 03:18:50 -0500

    Updates

--- templates/todo_starter/deploymentStart.js
+++ templates/todo_starter/deploymentStart.js
@@ -24,17 +24,17 @@ app.use(express.json())// returns middleware that only parses JSON
 app.use(express.static('public'))
 
 app.use(function(req, res, next) {
-res.header(“Access-Control-Allow-Origin”, “*”);
-res.header(“Access-Control-Allow-Methods”, “GET,PUT,POST,DELETE”);
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 res.header(
-“Access-Control-Allow-Headers”,
-“Origin, X-Requested-With, Content-Type, Accept”
+'Access-Control-Allow-Headers',
+'Origin, X-Requested-With, Content-Type, Accept'
 );
 next();
 });
 
 // Routes
-const todosController = require('./backendSrc/controllers/todos.js')
+const todosController = require('./backEndSrc/controllers/todos.js')
 app.use('/api/todos', todosController)
 
 // this will catch any route that doesn't exist
