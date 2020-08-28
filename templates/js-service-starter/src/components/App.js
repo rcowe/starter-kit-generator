05b48c9c215308381385f5de826035e8b3183221
Arthur <arthur.bernier@blavity.com>
2020-08-28 10:09:24 -0400

    added stuff

---
+++ templates/js-service-starter/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function App(props) {
+	const [name, upDateName] = useState('Hello World');
+	return (
+		<div className="Page-wrapper">
+			<h2>Arete App</h2>
+			<h3>{name}</h3>
+		</div>
+	);
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<App />, app);
