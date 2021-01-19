91e97161657629a666a20e21870c59efeeb8a43b
Arthur <arthur.bernier@blavity.com>
2021-01-19 16:06:31 -0500

    Minor Changes

--- templates/get-started/src/components/App.js
+++ templates/get-started/src/components/App.js
@@ -4,7 +4,7 @@ export default function App(props) {
 	const [name, updateName] = useState('Arthur');
 	return (
 		<div className="Page-wrapper">
-			<h2>Arete App</h2>
+			<h2>Ruth App</h2>
 			<button onClick={e => updateName(e.target.value)} value={name + 's'}>
 				Change
 			</button>
